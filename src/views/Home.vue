8fcb4455e7aa48cfc7f837cb3c2e62374973c622
Sergiu Ivasenco <contact@softinite.com>
2022-02-04 20:46:55 -0500

    Added missing files

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1 class="owen-title">Owen Community Learning Centre</h1>
+    <CovidQuestionnaire/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CovidQuestionnaire from '@/components/CovidQuestionnaire.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CovidQuestionnaire
+  }
+}
+</script>
